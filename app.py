a96eadf6b559258207ecb8f3123a69f76ba5cd88
jloovet <jloovet@gmail.com>
2020-12-31 17:15:01 +0100

    now sends and recieves votes

--- app.py
+++ app.py
@@ -17,31 +17,20 @@ def index():
     return render_template('index.html', async_mode=socket_.async_mode)
 
 
-@socket_.on('my_event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-
-@socket_.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
-
-
-@socket_.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
-
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
+@socket_.on('vote', namespace='/api')
+def vote_handler(vote_data):
+    print("printing voting ", vote_data)
+    #send to all using broadcast
+    emit('vote_result',
+    [
+        {"voters" : ["kssk", "dsdsd"]},        
+        {"voters" : ["kssk"]},
+        {"voters" : ["kssk", "dsdsd"]},        
+        {"voters" : ["kssk", "dsdsd", "sass"]},
+        {"voters" : []},        
+        {"voters" : ["!dsd#e#!sd"]}
+    ],
+   broadcast=True)
 
 
 if __name__ == '__main__':
