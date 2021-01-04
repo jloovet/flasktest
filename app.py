459db409e0359b665f322f025f571a6ad9306d2c
jloovet <jloovet@gmail.com>
2021-01-04 16:31:30 +0100

    try with port 8000, instead of 5000

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def update_clients() :
     emit('vote_result', room.to_json_obj(), broadcast=True)
 
 if __name__ == '__main__':
-    socket_.run(app, debug=True, host="0.0.0.0")
+    socket_.run(app, debug=True, host="0.0.0.0", port=8000)
